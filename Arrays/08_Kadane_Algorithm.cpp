43827f64e837030f4ded41fd3d46825a7649cce9
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-11 09:30:51 +0530

    Update 08_Kadane_Algorithm.cpp

--- Arrays/08_Kadane_Algorithm.cpp
+++ Arrays/08_Kadane_Algorithm.cpp
@@ -1,13 +1,10 @@
 long long maxSubarraySum(int arr[], int n){
         long long csum = arr[0]; long long osum = arr[0];
         for(int i=1; i<n; i++){
-            if(arr[i] <= (osum + arr[i])){
-                osum += arr[i];
+            if(osum < 0){
+                osum = 0;
             }
-            else{
-                osum = arr[i];
-            }
-            
+            osum += arr[i];
             if(csum < osum){
                 csum = osum;
             }
