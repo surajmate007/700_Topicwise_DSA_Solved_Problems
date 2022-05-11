779e174d67473f7db45b2cbc6879b087f13a1a43
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-11 11:14:06 +0530

    Create 17_find_all_pairs_on_integer_array_whose_sum_equal_to_given_number.cpp

---
+++ Arrays/17_find_all_pairs_on_integer_array_whose_sum_equal_to_given_number.cpp
@@ -0,0 +1,18 @@
+// using unordered map, we will store required difference to get the sum inside the map and then keep 
+// on searching for this value in our array.
+ 
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        unordered_map<int, int> mp; int ans = 0;
+        for(int i=0; i<n; i++){
+            if(mp[arr[i]] > 0){
+                ans += mp[arr[i]];
+            }
+            if(arr[i] < k){
+                mp[k-arr[i]]++;
+            }
+        }
+        return ans;
+    }
+};
