959b4385e6788ae15b09188d0c4aa232d8c61759
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-12 09:24:10 +0530

    Update 09_Minimize_the_Heights_II.cpp

--- Arrays/09_Minimize_the_Heights_II.cpp
+++ Arrays/09_Minimize_the_Heights_II.cpp
@@ -3,7 +3,7 @@ class Solution {
     int getMinDiff(int arr[], int n, int k) {
         sort(arr, arr+n);
         int ans = arr[n-1] - arr[0];
-        int smallest = arr[0]+k; int largest = arr[n-1]-k;
+        int smallest = arr[0]+k; int largest = arr[n-1]-k;  // smallest and largest reepresent smallest and largest height possible in complete array
         int mi, ma;
         for(int i=0; i<n-1; i++){
             mi = min(smallest, arr[i+1]-k);
