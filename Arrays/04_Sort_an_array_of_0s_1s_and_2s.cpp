827a59ce0ec85c9616e3a85b4c42deb2dd7bcd46
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-12 09:16:49 +0530

    Update 04_Sort_an_array_of_0s_1s_and_2s.cpp

--- Arrays/04_Sort_an_array_of_0s_1s_and_2s.cpp
+++ Arrays/04_Sort_an_array_of_0s_1s_and_2s.cpp
@@ -1,3 +1,5 @@
+// using 3 pointer method where the middle pointer will always look for maintaining 0 below its index and 2 above its index.
+
 void sort012(int a[], int n){
     int p=0; int q=0; int r=n-1;
     while(q <= r){
