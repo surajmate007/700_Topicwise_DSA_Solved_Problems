dc72f159f820f14b327ad0687f6e173db60d7469
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-12 09:33:24 +0530

    Update 12_Merge_Without_Extra_Space.cpp

--- Arrays/12_Merge_Without_Extra_Space.cpp
+++ Arrays/12_Merge_Without_Extra_Space.cpp
@@ -1,3 +1,6 @@
+// keeping one pointer at start of first array, second pointer at start of 2nd array and 3rd pointer at end of first array.
+// compare elements at index i and j and then swap elemnts at index j and k.
+
 class Solution{
 public:
 	void merge(int arr1[], int arr2[], int n, int m) {
