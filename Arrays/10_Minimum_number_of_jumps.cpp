cc03ce65b1ed288b8b40a351677198c326e74144
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-12 09:27:00 +0530

    Update 10_Minimum_number_of_jumps.cpp

--- Arrays/10_Minimum_number_of_jumps.cpp
+++ Arrays/10_Minimum_number_of_jumps.cpp
@@ -1,22 +1,28 @@
-class Solution{
-  public:
-    int minJumps(int arr[], int n){
-        int pos = 0; int count = 0; int last = 0;
-        while(pos < n-1){
-            if(arr[pos] == 0){
-                return -1;
-            }
-            int mx = 0;
-            for(int i = last+1; i<= last+arr[pos]; i++){
-                if(arr[i] > mx){
-                    mx = arr[i];
-                    last = i;
+// finding maximum reach possible after every index and decrementing steps by 1
+
+int minJumps(int arr[], int n){
+        if(n==1){
+            return 0;
+        }
+        else if(arr[0] == 0){
+            return -1;
+        }
+        else{
+            int maxR = arr[0]; int step = arr[0]; int jumps = 1;
+            for(int i=1; i<n; i++){
+                if(i == n-1){
+                    return jumps;
+                }
+                maxR = max(maxR, i+arr[i]);
+                step--;
+                if(step == 0){
+                    jumps++;
+                    if(i >= maxR){
+                        return -1;
+                    }
+                    step += maxR-i;
                 }
             }
-            
-            pos += mx;
-            count++;
         }
-        return count+1;
+        
     }
-};
