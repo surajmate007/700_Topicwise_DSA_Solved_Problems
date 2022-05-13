1d374edc8233e0c957220863a359f6f189c50a21
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-13 09:07:07 +0530

    Update 03_Kth_smallest_element.cpp

--- Arrays/03_Kth_smallest_element.cpp
+++ Arrays/03_Kth_smallest_element.cpp
@@ -1,3 +1,5 @@
+// for kth smallest element we use maxheap(priority queue) and for kth largest element we use minheap.
+
 int kthSmallest(int arr[], int l, int r, int k){
         priority_queue<int> pq;
         for(int i=0; i<=r; i++){
