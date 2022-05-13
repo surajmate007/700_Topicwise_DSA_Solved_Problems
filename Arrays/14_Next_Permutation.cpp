049897e32b99d0f1510273814d940c74ddba1840
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-13 09:40:02 +0530

    Update 14_Next_Permutation.cpp

--- Arrays/14_Next_Permutation.cpp
+++ Arrays/14_Next_Permutation.cpp
@@ -1,3 +1,7 @@
+// first find nums[i] < nums[i+1] and mark i as index1. if idx1 == -1 then simply reverse that array.
+// Then find nums[i] > nums[index1] then mark that i as index2.
+// swap elements at index1 and index2 and then reverse the array after index1.
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
