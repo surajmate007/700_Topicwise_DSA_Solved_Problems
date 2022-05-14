4206748bcd0c9713001aa9718abeda01773e9b5e
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-14 08:35:19 +0530

    Update 05_Move_all_negative_elements_to_end.cpp

--- Arrays/05_Move_all_negative_elements_to_end.cpp
+++ Arrays/05_Move_all_negative_elements_to_end.cpp
@@ -24,3 +24,25 @@ void segregateElements(int a[],int n){
 
 // if we dont need to maintain the order of elements then we can simply sort the array and use 2 pointer approach where one is staerting from +ve and 
 // other from -ve.
+
+// using two pointer approach to arrange the elements in given order 
+void arrange(int a[], int n) {
+    int negi = 0;
+    int posi = n - 1;
+
+    while (negi <= posi) {
+        if (a[negi] < 0 && a[posi] < 0) {
+            negi++;
+        }
+        else if (a[negi] > 0 && a[posi] < 0) {
+            swap(a[negi], a[pos]);
+            negi++;
+            pos--;
+        }
+        else if (a[negi] > 0 && a[posi] > 0) posi--;
+        else {
+            negi++;
+            posi--;
+        }
+    }
+}
