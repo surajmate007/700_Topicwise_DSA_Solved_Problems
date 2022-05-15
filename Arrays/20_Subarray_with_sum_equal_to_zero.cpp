77fa15f1c946ab88d79cc7dfee6da82005d420f2
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-15 09:25:42 +0530

    Revise Algorithm

--- Arrays/20_Subarray_with_sum_equal_to_zero.cpp
+++ Arrays/20_Subarray_with_sum_equal_to_zero.cpp
@@ -1,5 +1,6 @@
 // If the prefix sum is repeating then it is confirmed that the sum is equal to zero. 
 
+
 bool subArrayExists(int arr[], int n){
         int sum = 0;
         unordered_map<int, int> mp;
