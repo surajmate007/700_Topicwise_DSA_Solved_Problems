c11c698a4a1edbc0ec0ff3a11a83a210d9bbd3bd
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-15 12:07:38 +0530

    similar to LCS

---
+++ 14_Dynamic_Programming/14_Longest_Repeated_Subsequence.cpp
@@ -0,0 +1,21 @@
+// it is also similar to LCS, just we will not increment subsequence count when the index of both the characters in string are same.
+
+int LongestRepeatingSubsequence(string str){
+		    int n = str.length();
+		    int dp[n+1][n+1];
+		    for(int i=0; i<=n; i++){
+		        for(int j=0; j<=n; j++){
+		            if(i==0 or j==0){
+		                dp[i][j] = 0;
+		            }
+		            else if(str[i-1] == str[j-1] and i!=j){
+		                dp[i][j] = 1 + dp[i-1][j-1];
+		            }
+		            else{
+		                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+		            }
+		        }
+		    }
+		    
+		    return dp[n][n];
+}
