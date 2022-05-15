c306d4f347ecb731ba33cd64ed34040cccfdd746
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-15 10:41:37 +0530

    Update 03_Permutation_Coefficient_Problem.cpp

--- 14_Dynamic_Programming/03_Permutation_Coefficient_Problem.cpp
+++ 14_Dynamic_Programming/03_Permutation_Coefficient_Problem.cpp
@@ -1,5 +1,31 @@
 // we are simply implementing the formula relation::      nPr = (n-1)Pr + r * (n-1)P(r-1)
 
+int permutationCoeff(int n, int k)
+{
+    int P[n + 1][k + 1];
+ 
+    // Calculate value of Permutation
+    // Coefficient in bottom up manner
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= std::min(i, k); j++)
+        {
+            // Base Cases
+            if (j == 0)
+                P[i][j] = 1;
+            
+            else
+                P[i][j] = P[i - 1][j] + (j * P[i - 1][j - 1]);
+ 
+            // This step is important
+            // as P(i,j)=0 for j>i
+            P[i][j + 1] = 0;
+        }
+    }
+    return P[n][k];
+}
+
+
 class Solution{
 public:
     const long long mod = 1e9 + 7;
@@ -17,7 +43,7 @@ public:
     
         for (int i = 1;i < n + 1;i++) {
             for (int j = 1;j <= min(i, r);j++) {
-                memo[i][j] = (memo[i - 1][j] % mod + (j * memo[i - 1][j] % mod)) % mod;
+                memo[i][j] = (memo[i - 1][j] % mod + (j * memo[i - 1][j-1] % mod)) % mod;
             }
         }
         return memo[n][r] % mod;
