18e3c20d8f0797878b4c385ae93f8c08bf7f155d
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-15 09:36:37 +0530

    Important to revise for 2 pointer approach

--- Arrays/27_Find_the_triplet_that_sum_to_a_given_value.cpp
+++ Arrays/27_Find_the_triplet_that_sum_to_a_given_value.cpp
@@ -1,6 +1,7 @@
 // Since expected TC is O(N2), hence we can sort the array which is less than N2 and then we will subtract one value from X and
 // using 2 pointer method on sorted array we will try to find the 2 elements which satisfy the remaining sum.
 
+
 bool find3Numbers(int arr[], int n, int X){
         sort(arr, arr+n);
         for(int i=0; i<n; i++){
