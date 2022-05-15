c9e778ca697ee23be73022fa5f5e490e51ed3f1e
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-15 09:52:24 +0530

    Easy

--- Arrays/33_Minimum_no_of_operations_required_to_make_an_array_palindrome.cpp
+++ Arrays/33_Minimum_no_of_operations_required_to_make_an_array_palindrome.cpp
@@ -1,3 +1,15 @@
 // using 2 pointer approach
 
-solved in handwritten notes
+
+int minOperations(int arr[], int n){
+    int ans = 0; int i=0; int j=n-1;
+    while(i<=j){
+        if(arr[i] == arr[j]){
+          i++; j--;
+        }
+        else if(arr[i]<arr[j]){i++; arr[i] += arr[i-1]; ans++;}
+        else {j--; arr[j] += arr[j+1]; ans++;}
+    }
+  
+  return ans;
+}
