dc910074525e2ca2864af93cd5dd8c689df3a5ec
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-16 09:27:57 +0530

    like unbounded knapsack

--- 14_Dynamic_Programming/00_Coin_change_problem.cpp
+++ 14_Dynamic_Programming/00_Coin_change_problem.cpp
@@ -2,28 +2,24 @@
 
 class Solution{
 	public:
-	int minimumCost(int cost[], int N, int W) { 
-        long long dp[N+1][W+1];
-        for(int i=0; i<=N; i++){
-            for(int j=0; j<=W; j++){
-                if(i==0){
-                    dp[i][j] = INT_MAX;
-                }
-                else if(j==0){
-                    dp[i][j] = 0;
-                }
-                else if(cost[i-1] == -1){
-                    dp[i][j] = dp[i-1][j];
-                }
-                else if(i <= j){
-                    dp[i][j] = min(cost[i-1]+dp[i][j-i], dp[i-1][j]);
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        
-        return dp[N][W]==INT_MAX ? -1:dp[N][W];
-	} 
+	long long int count(int S[], int m, int n) {
+		long long dp[m+1][n+1];
+		for(int i=0; i<m+1; i++){
+		    for(int j=0; j<n+1; j++){
+			if(j==0){
+			    dp[i][j] = 1;
+			}
+			else if(i==0){
+			    dp[i][j] = 0;
+			}
+			else if(S[i-1] <= j){
+			    dp[i][j] = dp[i][j-S[i-1]] + dp[i-1][j];
+			}
+			else{
+			    dp[i][j] = dp[i-1][j];
+			}
+		    }
+		}
+        return dp[m][n];
+    }
 };
