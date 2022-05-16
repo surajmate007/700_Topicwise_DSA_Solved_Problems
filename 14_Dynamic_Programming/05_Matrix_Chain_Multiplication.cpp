ffb75988e95b302ec875f1cd03c3638b944f2ee0
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-16 09:41:57 +0530

    Update 05_Matrix_Chain_Multiplication.cpp

--- 14_Dynamic_Programming/05_Matrix_Chain_Multiplication.cpp
+++ 14_Dynamic_Programming/05_Matrix_Chain_Multiplication.cpp
@@ -25,7 +25,7 @@ public:
     int dp[1001][1001];
     
     int getmcm(int arr[], int i, int j){
-        if(i==j){
+        if(i>=j){
             return 0;
         }
         if(dp[i][j] != -1){
@@ -46,37 +46,39 @@ public:
 };
 
 // Top-Down Approach:
+class Solution{
+public:
+    int getmcm(int p[], int n){
 
-int MatrixChainOrder(int p[], int n){
- 
-    /* For simplicity of the program, one
-    extra row and one extra column are
-    allocated in dp[][]. 0th row and 0th
-    column of dp[][] are not used */
-    int dp[n][n];
- 
-    int i, j, k, L, q;
- 
-    /* m[i, j] = Minimum number of scalar
-    multiplications needed to compute the
-    matrix A[i]A[i+1]...A[j] = A[i..j] where
-    dimension of A[i] is p[i-1] x p[i] */
- 
-    // cost is zero when multiplying one matrix.
-    for (i = 1; i < n; i++)
-        m[i][i] = 0;
- 
-    
-    for (L = 2; L < n; L++){
-        for (i = 1; i < n - L + 1; i++){
-            j = i + L - 1;
-            m[i][j] = INT_MAX;
-            for (k = i; k < j; k++){
-                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                if (q < m[i][j])
-                    m[i][j] = q;
+        /* For simplicity of the program, one
+        extra row and one extra column are
+        allocated in dp[][]. 0th row and 0th
+        column of dp[][] are not used */
+        int dp[n][n];
+
+        int i, j, k, L, q;
+
+        /* m[i, j] = Minimum number of scalar
+        multiplications needed to compute the
+        matrix A[i]A[i+1]...A[j] = A[i..j] where
+        dimension of A[i] is p[i-1] x p[i] */
+
+        // cost is zero when multiplying one matrix.
+        for (i = 1; i < n; i++)
+            m[i][i] = 0;
+
+
+        for (L = 2; L < n; L++){
+            for (i = 1; i < n - L + 1; i++){
+                j = i + L - 1;
+                m[i][j] = INT_MAX;
+                for (k = i; k < j; k++){
+                    q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (q < m[i][j])
+                        m[i][j] = q;
+                }
             }
         }
-    }
     return m[1][n - 1];
-}
+ }
+};
