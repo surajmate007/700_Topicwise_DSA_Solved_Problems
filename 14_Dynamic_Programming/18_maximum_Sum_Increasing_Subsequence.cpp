26bb5f3cc92a35ce4fe8d50a257f2ad9f6fc7d80
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-17 10:17:53 +0530

    same as LIS only focus on max sum

--- 14_Dynamic_Programming/18_maximum_Sum_Increasing_Subsequence.cpp
+++ 14_Dynamic_Programming/18_maximum_Sum_Increasing_Subsequence.cpp
@@ -1,21 +1,18 @@
 int maxSumIS(int arr[], int n){  
 	    int dp[n];
+	    int ans = 0;
 	    for(int i=0; i<n; i++){
 	        dp[i] = arr[i];
 	    }
+	    
 	    
 	    for(int i=1; i<n; i++){
 	        for(int j=0; j<i; j++){
 	            if(arr[j] < arr[i] and dp[i] < dp[j]+arr[i]){
 	                dp[i] = dp[j] + arr[i];
+			ans = max(ans, dp[i]);
 	            }
 	        }
 	    }
-	    
-	    int ans = INT_MIN;
-	    for(int i=0; i<n; i++){
-	        ans = max(ans, dp[i]);
-	    }
-	   
 	    return ans;
-	}  
+}  
