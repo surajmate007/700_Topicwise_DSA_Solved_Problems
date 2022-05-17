0274805d6a3fe1082a6b84961a96461c7a00c26e
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-17 13:04:02 +0530

    revise frequently

---
+++ 14_Dynamic_Programming/22 _Egg_Dropping_Problem.cpp
@@ -0,0 +1,61 @@
+// Simple Recursive Approach (gut giving TLE):
+// Here n-- no. of eggs and k -- no. of floors.
+// when no of eggs is only 1 then we will start from ground floor and try every single floor by droping the egg. Hence attempts required will be k.
+// when we have 0 floors or only glound floor then the attempts will be no. of floors.
+// now for more number of floors we will drop egg from every single floor and check whether it broke or not.
+// if egg is btoken then we will reduce eggs by 1 and floor by 1.
+// if not broken then we will check it in upper floors.
+
+int eggDrop(int n, int k) {
+       if(n==1){return k;}
+       if(k==0 or k==1){
+           return k;
+       }
+       int ans = INT_MAX;
+       
+       for(int i=1; i<=k; i++){
+           int temp = 1 + max(eggDrop(n-1, i-1), eggDrop(n, k-i));
+           ans = min(ans, temp);
+       }
+       
+       return ans;
+}
+
+// Egg Dropping Memoized:
+
+int dp[201][201];
+    int getans(int n, int k){
+        if(n==1 or k==0 or k==1){
+            return k;
+        }
+        if(dp[n][k] != -1){
+            return dp[n][k];
+        }
+        int mn = INT_MAX;
+        
+        for(int i=1; i<=k; i++){
+            int up, down;
+            if(dp[n-1][i-1] != -1){
+                down = dp[n-1][i-1];
+            }
+            else{
+                down = getans(n-1, i-1);
+            }
+            
+            if(dp[n][k-i] != -1){
+                up = dp[n][k-i];
+            }
+            else{
+                up = getans(n, k-i);
+            }
+            
+            int temp = 1 + max(up, down);
+            mn = min(mn, temp);
+        }
+        return dp[n][k] = mn;
+    }
+    
+    int eggDrop(int n, int k) {
+       memset(dp, -1, sizeof(dp));
+       return getans(n, k);
+}
