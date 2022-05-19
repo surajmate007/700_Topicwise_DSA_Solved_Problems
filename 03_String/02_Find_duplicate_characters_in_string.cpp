99805077ed75aafaf88d83805a706336192a93a0
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-19 13:25:09 +0530

    easy

---
+++ 03_String/02_Find_duplicate_characters_in_string.cpp
@@ -0,0 +1,60 @@
+// we can simply use hashing where we will use array and store the count of chars in the corresponding index of character:
+
+void fillCharCounts(char *str, int *count)
+    {
+        int i;
+        for (i = 0; *(str + i); i++)
+        count[*(str + i)]++;
+    }
+ 
+    /* Print duplicates present
+    in the passed string */
+    void printDups(char *str)
+    {
+         
+        // Create an array of size 256 and fill
+        // count of every character in it
+        int *count = (int *)calloc(NO_OF_CHARS,
+                                      sizeof(int));
+        fillCharCounts(str, count);
+ 
+        // Print characters having count more than 0
+        int i;
+        for (i = 0; i < NO_OF_CHARS; i++)
+        if(count[i] > 1)
+            printf("%c, count = %d \n", i, count[i]);
+ 
+        free(count);
+    }
+
+
+// another simple approach is using haspmap:
+
+void printDups(string str)
+{
+    map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;
+    }
+ 
+    for (auto it : count) {
+        if (it.second > 1)
+            cout << it.first << ", count = " << it.second
+                 << "\n";
+    }
+}
+
+// we can also use unordered_map for the same purpose:
+
+void printDups(string str){
+    unordered_map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;  //increase the count of characters by 1
+    }
+  
+    for (auto it : count) {   //iterating through the unordered map
+        if (it.second > 1)   //if the count of characters is greater then 1 then duplicate found
+            cout << it.first << ", count = " << it.second
+                 << "\n";
+    }
+}
