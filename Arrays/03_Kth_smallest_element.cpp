6522c5d4a6890517c753777c05581743163c365c
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-19 08:22:39 +0530

    how to implement mishap

--- Arrays/03_Kth_smallest_element.cpp
+++ Arrays/03_Kth_smallest_element.cpp
@@ -10,3 +10,17 @@ int kthSmallest(int arr[], int l, int r, int k){
         }
         return pq.top();
 }
+
+
+// for kth largest elemengt we can implement minheap as below:
+
+int kthLargest(int arr[], int l, int r, int k){
+        priority_queue <int, vector<int>, greater<int> > pq;            // this line will create minheap.
+        for(int i=0; i<=r; i++){
+            pq.push(arr[i]);
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        return pq.top();
+}
