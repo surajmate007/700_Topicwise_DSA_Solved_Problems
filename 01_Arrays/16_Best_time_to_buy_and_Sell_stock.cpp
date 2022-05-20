b60a6f3bae61e93f629b638dcca10766efe710f4
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-20 08:38:25 +0530

    easy keep track of min so far

---
+++ 01_Arrays/16_Best_time_to_buy_and_Sell_stock.cpp
@@ -0,0 +1,39 @@
+// using 2 array approach one stores min and other stores max element.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size()-1;
+        vector<int> mn(n+1, 0);
+        vector<int> mx(n+1, 0);
+        mn[0] = prices[0]; mx[n] = prices[n];
+        for(int i=1; i<=n; i++){
+            mn[i] = min(mn[i-1], prices[i]);
+            mx[n-i] = max(mx[n-i+1], prices[n-i]);
+        }
+        int ans = 0;
+        
+        for(int i=0; i<=n; i++){
+            ans = max(ans, (mx[i] - mn[i]));
+        }
+        
+        return ans;
+    }
+};
+
+// keeping track of min value and subtracting it from current array values and finding the maximum difference.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn = prices[0];
+        int ans=0; int diff=0;
+        for(int i=0; i<prices.size(); i++){
+            mn = min(mn, prices[i]);
+            diff = prices[i]-mn;
+            ans = max(ans, diff);
+        }
+        
+        return ans;
+    }
+};
