30f13636cae4ed9417a4101aa95e527821c5f8f5
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-20 08:26:39 +0530

    easy

--- 01_Arrays/00_Reverse_an_Array.cpp
+++ 01_Arrays/00_Reverse_an_Array.cpp
@@ -1,1 +1,30 @@
 // this is the simple code to reverse an array
+
+// TC: O(N) iterative approach
+void rvereseArray(int arr[], int start, int end)
+{
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+
+// ----------------------------------------------------------------------------------------------------------------------- //
+// TC: O(N) recursive approach
+void rvereseArray(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    // Recursive Function calling
+    rvereseArray(arr, start + 1, end - 1);
+}
