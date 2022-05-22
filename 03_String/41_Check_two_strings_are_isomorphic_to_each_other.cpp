ed3ef500567a9d5c860d860dbde58d3cff2bb586
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-22 16:23:58 +0530

    revise

---
+++ 03_String/41_Check_two_strings_are_isomorphic_to_each_other.cpp
@@ -0,0 +1,27 @@
+// easy solution:
+
+class Solution{
+    public:
+    //Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2){
+        int n = str1.size(); int m = str2.size();
+        if(n != m){
+            return false;
+        }
+        int arr1[256] = {0};
+        int arr2[256] = {0};
+        
+        for(int i=0; i<n; i++){
+            arr1[str1[i]]++;
+            arr2[str2[i]]++;
+        }
+        
+        for(int i=0; i<m; i++){
+            if(arr1[str1[i]] != arr2[str2[i]]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
