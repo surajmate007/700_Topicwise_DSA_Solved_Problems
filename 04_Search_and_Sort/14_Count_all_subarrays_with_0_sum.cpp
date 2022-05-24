8c44086edbe31c80ce1acca2e33396e12fcedb7e
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-24 18:13:46 +0530

    revise

--- 04_Search_and_Sort/14_Count_all_subarrays_with_0_sum.cpp
+++ 04_Search_and_Sort/14_Count_all_subarrays_with_0_sum.cpp
@@ -5,11 +5,12 @@ class Solution{
     //Function to count subarrays with sum equal to 0.
     ll findSubarray(vector<ll> arr, int n ) {
         unordered_map<long long, long long> mp;
-        mp[0] = 1;
         long long ans = 0; long long sum = 0;
-        
         for(auto e : arr){
             sum += e;
+            if(sum == 0){
+                ans++;
+            }
             if(mp.find(sum) != mp.end()){
                 ans += mp[sum];
             }
@@ -19,3 +20,56 @@ class Solution{
         return ans;
     }
 };
+
+
+
+// If we want to print the sub arrays then we simply modify the code as:
+
+vector< pair<int, int> > findSubArrays(int arr[], int n)
+{
+    // create an empty map
+    unordered_map<int, vector<int> > map;
+
+    // create an empty vector of pairs to store
+    // subarray starting and ending index
+    vector <pair<int, int>> out;
+
+    // Maintains sum of elements so far
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        // add current element to sum
+        sum += arr[i];
+
+        // if sum is 0, we found a subarray starting
+        // from index 0 and ending at index i
+        if (sum == 0)
+            out.push_back(make_pair(0, i));
+
+        // If sum already exists in the map there exists
+        // at-least one subarray ending at index i with
+        // 0 sum
+        if (map.find(sum) != map.end())
+        {
+            // map[sum] stores starting index of all subarrays
+            vector<int> vc = map[sum];
+            for (auto it = vc.begin(); it != vc.end(); it++)
+                out.push_back(make_pair(*it + 1, i));
+        }
+
+        // Important - no else
+        map[sum].push_back(i);
+    }
+
+    // return output vector
+    return out;
+}
+
+// Utility function to print all subarrays with sum 0
+void print(vector<pair<int, int>> out)
+{
+    for (auto it = out.begin(); it != out.end(); it++)
+        cout << "Subarray found from Index " <<
+        it->first << " to " << it->second << endl;
+}
