34459bf1df0c0cece8ec47677eab9b674f4c4e10
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-24 10:33:13 +0530

    revise indexing

---
+++ 04_Search_and_Sort/00_First_and_last_position_of_an_element_in_sorted_array.cpp
@@ -0,0 +1,73 @@
+/*
+    simple iteration
+    TC: O(N)
+*/
+vector<int> find(int arr[], int n, int x)
+{
+    // code here
+    int i = n, j = 0;
+    bool flag = true;
+    for (int k = 0;k < n;k++) {
+        if (arr[k] == x) {
+            i = min(i, k);
+            j = max(j, k);
+            flag = false;
+        }
+    }
+
+    // if ele is not present
+    if (flag) return { -1, -1 };
+
+    // if ele is present
+    return { i, j };
+}
+
+
+
+/*
+    using binary search
+    TC: O(logn)
+    SC: O(1)
+*/ 
+
+// Important points to note:
+// 1. For first occurance of an element in else part we have to do hi = mid-1;
+// 2. For last occurance of an element in lese part we have to do lo = mid+1;
+
+
+vector<int> find(int arr[], int n, int x){
+    int lo=0; int hi=n-1; vector<int> ans;
+
+    while(lo <= hi){
+        int mid = (lo+hi)/2;
+        if(arr[mid] < x){
+           lo = mid+1;
+        }
+        else{
+            hi = mid-1;
+        }
+    }
+    ans.push_back(lo);
+    
+    lo = 0; hi=n-1;
+    
+    while(lo <= hi){
+        int mid = (lo+hi)/2;
+        if(arr[mid] > x){
+            hi = mid-1;
+        }
+        else{
+            lo = mid+1;
+        }
+    }
+    
+    if(hi < ans.back()){
+        ans.pop_back();
+        ans.push_back(-1);
+        ans.push_back(-1);
+    }
+    else{
+        ans.push_back(hi);
+    }
+    return ans;
+}
