30dc435a44d0deca440aa31188b064bf40f034a3
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-27 18:32:20 +0530

    easy but revise

--- 05_Linked_List/05_Remove_Duplicates_in_Sorted_Linked_List.cpp
+++ 05_Linked_List/05_Remove_Duplicates_in_Sorted_Linked_List.cpp
@@ -13,3 +13,41 @@ void removeDuplicates(struct Node* head){
         }
     }
 }
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------- //
+/*
+    using recursion
+*/
+void removeDuplicates(Node* head)
+{
+    /* Pointer to store the pointer of a node to be deleted*/
+    Node* to_free;
+
+    /* do nothing if the list is empty */
+    if (head == NULL)
+        return;
+
+    /* Traverse the list till last node */
+    if (head->next != NULL)
+    {
+
+        /* Compare head node with next node */
+        if (head->data == head->next->data)
+        {
+            /* The sequence of steps is important.
+              to_free pointer stores the next of head
+             pointer which is to be deleted.*/
+            to_free = head->next;
+            head->next = head->next->next;
+            free(to_free);
+            removeDuplicates(head);
+        }
+        else /* This is tricky: only
+        advance if no deletion */
+        {
+            removeDuplicates(head->next);
+        }
+    }
+}
