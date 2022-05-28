da9019b583053e7cbefdcd4c5d6611a55b9c1ad8
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-28 11:05:29 +0530

    revise

---
+++ 05_Linked_List/10_Intersection_of_two_Sorted_LL.cpp
@@ -0,0 +1,28 @@
+// using 2 pointer approach solved by me.
+// Since it is sorted.
+
+Node* findIntersection(Node* head1, Node* head2){
+    Node* temp1 = head1;
+    Node* temp2 = head2;
+    Node* start = new Node(0);
+    Node* point = start;
+    
+    while(temp1 != NULL and temp2 != NULL){
+        if(temp1->data == temp2->data){
+            Node* nd = new Node(temp1->data);
+            start->next = nd;
+            start = nd;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else if(temp1->data < temp2->data){
+            temp1 = temp1->next;
+        }
+        else{
+            temp2 = temp2->next;
+        }
+    }
+    start->next = NULL;
+    return point->next;
+}
+
