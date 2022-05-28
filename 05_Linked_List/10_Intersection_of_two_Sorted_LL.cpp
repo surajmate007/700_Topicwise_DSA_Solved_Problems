963367af9d013902915c187b42137f169cf4474f
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-28 11:14:58 +0530

    revise

--- 05_Linked_List/10_Intersection_of_two_Sorted_LL.cpp
+++ 05_Linked_List/10_Intersection_of_two_Sorted_LL.cpp
@@ -26,3 +26,34 @@ Node* findIntersection(Node* head1, Node* head2){
     return point->next;
 }
 
+// Some optimisation with use of less pointers.
+
+Node* findIntersection(Node* head1, Node* head2){
+    Node* temp1 = head1;
+    Node* temp2 = head2;
+    Node* head = NULL;
+    Node* point = NULL;
+    
+    while(temp1 != NULL and temp2 != NULL){
+        if(temp1->data == temp2->data){
+            Node* nd = new Node(temp1->data);
+            if(point != NULL){
+                point->next = nd;
+            }
+            if(head == NULL){
+                head = nd;
+            }
+            point = nd;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else if(temp1->data < temp2->data){
+            temp1 = temp1->next;
+        }
+        else{
+            temp2 = temp2->next;
+        }
+    }
+    point->next = NULL;
+    return head;
+}
