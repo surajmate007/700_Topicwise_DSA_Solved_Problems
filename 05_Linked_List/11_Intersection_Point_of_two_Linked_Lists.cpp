62575bb1dd69289ed20dbec95e5f34b5abb243b0
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-05-28 11:44:14 +0530

    revise frequently

---
+++ 05_Linked_List/11_Intersection_Point_of_two_Linked_Lists.cpp
@@ -0,0 +1,80 @@
+// We are first calculating the length of both the linked lists and then finding the difference in lenght.
+// Then we will move the head pointer of longer LL such that the remaining lenghts of bith the lists will become same.
+// Then we will start both the pointers until they meet each other and the first meeting point will be the point of intersection.
+
+int intersectPoint(Node* head1, Node* head2){
+    Node* temp1 = head1; Node* temp2 = head2;
+    int len1 = 1, len2 = 1;
+    while(temp1 != NULL){
+        temp1 = temp1->next;
+        len1++;
+    }
+    while(temp2 != NULL){
+        temp2 = temp2->next;
+        len2++;
+    }
+    
+    temp1 = head1; temp2 = head2;
+    if(len1 < len2){
+        while(len1 != len2){
+            temp2 = temp2->next;
+            len2--;
+        }
+    }
+    else{
+        while(len1 != len2){
+            temp1 = temp1->next;
+            len1--;
+        }
+    }
+    while(temp1 != temp2){
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return temp1->data;
+}
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------- //
+/*
+    using hashing
+    TC: O(max(m, n))    // size of linklist
+    SC: O(N)
+*/
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    unordered_set<Node*> s;
+    while (head1) {
+        s.insert(head1);
+        head1 = head1->next;
+    }
+    while (head2) {
+        if (s.find(head2) != s.end()) {
+            return head2->data;
+        }
+        head2 = head2->next;
+    }
+    return NULL;
+}
+
+
+
+// This solution will also work but giving TLE.
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2){
+    Node* temp1 = head1; Node* temp2 = head2;
+    while(temp1 != temp2){
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+        if(temp1 == NULL){
+            temp1 = head1;
+        }
+        if(temp2 == NULL){
+            temp2 = head2;
+        }
+    }
+    return temp1->data;
+}
