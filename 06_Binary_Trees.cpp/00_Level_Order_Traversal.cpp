ac2fa8cacda8a00a8555db2843cc0c72195d832c
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-02 12:21:51 +0530

    revise frequently

--- 06_Binary_Trees.cpp/00_Level_Order_Traversal.cpp
+++ 06_Binary_Trees.cpp/00_Level_Order_Traversal.cpp
@@ -25,3 +25,41 @@ class Solution{
         return ans;
     }
 };
+
+
+
+// Leetcode level order traversal with minor modifications:
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        
+        if(root == NULL){
+            return ans;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            vector<int>vec; int n = q.size();
+            while(n){
+                TreeNode* nd = q.front();
+                vec.push_back(nd->val);
+                q.pop();
+                if(nd->left != NULL){
+                    q.push(nd->left);
+                }
+                if(nd->right != NULL){
+                    q.push(nd->right);
+                }
+                n--;
+            }
+            ans.push_back(vec);
+        }
+        
+        return ans;
+    }
+};
+
