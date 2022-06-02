d9f30986adcb66cd488eb69678df8d591647c727
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-02 11:15:24 +0530

    revise frequently

--- 05_Linked_List/24_Rotate_DLL_in_Group_of_Given_Size.cpp
+++ 05_Linked_List/24_Rotate_DLL_in_Group_of_Given_Size.cpp
@@ -34,3 +34,61 @@ Node* reverseDLLInGroups(Node* head, int k){
     return head;
 }
 
+
+
+
+// Recursive approach:
+
+
+// function to insert a node at the beginging
+// of the Doubly Linked List
+void push(Node** head_ref, Node* new_node)
+{
+    // since we are adding at the beginning,
+    // prev is always NULL
+    new_node->prev = NULL;
+
+    // link the old list off the new node
+    new_node->next = (*head_ref);
+
+    // change prev of head node to new node
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = new_node;
+
+    // move the head to point to the new node
+    (*head_ref) = new_node;
+}
+
+// function to reverse a doubly linked list
+// in groups of given size
+Node* revListInGroupOfGivenSize(Node* head, int k)
+{
+    Node* current = head;
+    Node* next = NULL;
+    Node* newHead = NULL;
+    int count = 0;
+
+    // reversing the current group of k
+    // or less than k nodes by adding
+    // them at the beginning of list
+    // 'newHead'
+    while (current != NULL && count < k)
+    {
+        next = current->next;
+        push(&newHead, current);
+        current = next;
+        count++;
+    }
+
+    // if next group exists then making the desired
+    // adjustments in the link
+    if (next != NULL)
+    {
+        head->next = revListInGroupOfGivenSize(next, k);
+        head->next->prev = head;
+    }
+
+    // pointer to the new head of the
+    // reversed group
+    return newHead;
+}
