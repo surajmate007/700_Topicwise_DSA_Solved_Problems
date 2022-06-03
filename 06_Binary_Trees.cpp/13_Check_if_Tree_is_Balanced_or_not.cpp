97f127964556f75baec54b5f86a2975ada634efb
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-03 13:48:37 +0530

    revise frequently

---
+++ 06_Binary_Trees.cpp/13_Check_if_Tree_is_Balanced_or_not.cpp
@@ -0,0 +1,34 @@
+// We are first computing the right height and then left height of the node and then finding the difference.
+// iF THE DIFFERENCE ENCOUNTERED SO FAR IS GRATER THAN 1 THEN WE WILL RETURN -1.
+
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    int check(Node* root){
+        if(root == NULL){
+            return 0;
+        } 
+        
+        int lh = check(root->left);
+        int rh = check(root->right);
+        
+        if(lh == -1 or rh == -1){
+            return -1;
+        }
+        
+        if(abs(rh-lh) > 1){
+            return -1;
+        }
+        
+        return max(lh, rh) + 1;
+    }
+    
+    bool isBalanced(Node *root){
+        int ans = check(root);
+        if(ans == -1){
+            return false;
+        }
+        
+        return true;
+    }
+};
