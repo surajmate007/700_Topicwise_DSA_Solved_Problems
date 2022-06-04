7625c8e9196711e9331688fec602933a6423b4d7
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-04 08:59:29 +0530

    revise 2nd approach

--- 04_Search_and_Sort/08_Searching_in_an_array_where_adjacent_differ_by_atmost_k.cpp
+++ 04_Search_and_Sort/08_Searching_in_an_array_where_adjacent_differ_by_atmost_k.cpp
@@ -8,3 +8,28 @@ int search(int arr[], int n, int x, int k){
     }
     return -1;
 }    
+
+
+
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+int search(int arr[], int n, int x, int k){
+    // Traverse the given array starting from
+    // leftmost element
+    int i = 0;
+    while (i < n)
+    {
+        // If x is found at index i
+        if (arr[i] == x)
+            return i;
+
+        // Jump the difference between current
+        // array element and x divided by k
+        // We use max here to make sure that i
+        // moves at-least one step ahead.
+        i = i + max(1, abs(arr[i] - x) / k);
+    }
+
+    cout << "number is not present!";
+    return -1;
+}
