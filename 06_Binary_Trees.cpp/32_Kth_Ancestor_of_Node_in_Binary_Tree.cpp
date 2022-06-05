ad6f1d0fd03bd837df44fb6535a1b036596ecd12
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-05 22:08:45 +0530

    revise frequently

---
+++ 06_Binary_Trees.cpp/32_Kth_Ancestor_of_Node_in_Binary_Tree.cpp
@@ -0,0 +1,31 @@
+// temporary node to keep track of Node returned
+// from previous recursive call during backtrack
+Node* temp = NULL;
+ 
+// recursive function to calculate Kth ancestor
+Node* kthAncestorDFS(Node *root, int node , int &k)
+{  
+    // Base case
+    if (!root)
+        return NULL;
+     
+    if (root->data == node||
+       (temp =  kthAncestorDFS(root->left,node,k)) ||
+       (temp =  kthAncestorDFS(root->right,node,k)))
+    {  
+        if (k > 0)       
+            k--;
+         
+        else if (k == 0)
+        {
+            // print the kth ancestor
+            cout<<"Kth ancestor is: "<<root->data;
+             
+            // return NULL to stop further backtracking
+            return NULL;
+        }
+         
+        // return current node to previous call
+        return root;
+    }
+}
