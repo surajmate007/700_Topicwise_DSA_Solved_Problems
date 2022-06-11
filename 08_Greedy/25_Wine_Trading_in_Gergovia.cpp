4c7b2b533836d2b6510832fe566b7b1de043e10a
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-11 22:18:25 +0530

    revise

---
+++ 08_Greedy/25_Wine_Trading_in_Gergovia.cpp
@@ -0,0 +1,46 @@
+// This is the sliding window technique to solve this problem.
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin>>n;
+	while(n != 0){
+    	int a[n];
+    	for(int i=0; i<n; i++){
+    		cin>>a[i];
+    	}
+    	
+    	int i=0; int j=0; long long ans = 0;
+    	while(i<n and j<n){
+    	    
+    		if(a[j] >= 0){ j++; continue;}
+    		if(a[i] <= 0){ i++; continue;}
+    		
+    		if(abs(a[i]) < abs(a[j])){
+    			ans += (abs(a[i]) * (abs(j-i)));
+    			a[j] += a[i]; a[i] = 0;
+    			i++;
+    			continue;
+    		}
+    		
+    		else if(abs(a[j]) < abs(a[i])){
+    			ans += (abs(a[j]) * (abs(j-i)));
+    			a[i] += a[j]; a[j] = 0;
+    			j++;
+    			continue;
+    		}
+    		
+    		else{
+    			ans += (abs(a[j]) * abs(j-i));
+    			a[i] = 0; a[j] = 0;
+    			i++; j++;
+    		}
+    	}
+    	cout<<ans<<endl;
+    	
+    	cin>>n;
+	}
+	return 0;
+}
