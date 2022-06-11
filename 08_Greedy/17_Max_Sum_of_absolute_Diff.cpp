12a00dc9b458673c4d9fc7663d496aefe7938ef3
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-11 11:49:45 +0530

    easy

--- 08_Greedy/17_Max_Sum_of_absolute_Diff.cpp
+++ 08_Greedy/17_Max_Sum_of_absolute_Diff.cpp
@@ -1,23 +1,22 @@
 // Here we are first sorting the array and then arranging the bigger ekements in between smaller elements and then finding the answer.
 
-int maxSum(int arr[], int n){
+long long int maxSum(int arr[], int n){
     sort(arr, arr+n);
-    int i=0; nt j=n-1;
+    int i=0; int j=n-1;
 
     vector<int> vec;
     
-    while(i < j){
+    while(i <= j){
         vec.push_back(arr[i]);
         vec.push_back(arr[j]);
         i++; j--;
     }
-    
-    int ans = 0;
+  
+    long long int ans = 0;
     for(int i=0; i<n-1; i++){
-        ans += abs(arr[i] - arr[i+1]);
+        ans += abs(vec[i] - vec[i+1]);
     }
-    ans += abs(arr[0] - arr[n-1]);
+    ans += abs(vec[0] - vec[n-1]);
     
     return ans;
 }
-          
