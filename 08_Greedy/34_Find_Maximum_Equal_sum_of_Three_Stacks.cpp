f05e91e8a6416fc0b9520c0617a2c782962d5e29
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-12 18:45:21 +0530

    revise

--- 08_Greedy/34_Find_Maximum_Equal_sum_of_Three_Stacks.cpp
+++ 08_Greedy/34_Find_Maximum_Equal_sum_of_Three_Stacks.cpp
@@ -8,9 +8,13 @@ public:
     int maxEqualSum(int n1,int n2,int n3,vector<int> &s1,vector<int> &s2,vector<int> &s3){
         int i=n1-2; int j=n2-2; int k=n3-2; int ans = 0;
         int si = s1[n1-1]; int sj = s2[n2-1]; int sk = s3[n3-1];
-        int sa=0, sb=0, sc=0;
-        for(int i=0; i<n1; i++){
-            sa += s1[i];
+    
+        int sa = accumulate(s1.begin(),s1.end(),0);
+        int sb = accumulate(s2.begin(),s2.end(),0);
+        int sc = accumulate(s3.begin(),s3.end(),0);
+        
+        if(s1 == s2 ans s1 == s3){
+            return s1;
         }
         
         for(int i=0; i<n2; i++){
@@ -26,6 +30,7 @@ public:
         }
         
         while(i>=-1 and j>=-1 and k>=-1){
+            // cout<<si<<" "<<sj<<" "<<sk<<endl;
             if(si == sj and si==sk){
                 ans = si;
                 
@@ -53,3 +58,25 @@ public:
         return ans;
     }
 };
+
+
+// Better approach:
+
+int maxEqualSum(int n1,int n2,int n3,vector<int> &s1,vector<int> &s2,vector<int> &s3){
+       --n1;
+       --n2;
+       --n3;
+       reverse(s1.begin(),s1.end());
+       reverse(s2.begin(),s2.end());
+       reverse(s3.begin(),s3.end());
+       int sum1=accumulate(s1.begin(),s1.end(),0);
+       int sum2=accumulate(s2.begin(),s2.end(),0);
+       int sum3=accumulate(s3.begin(),s3.end(),0);
+       while(n1>=0 && n2>=0 && n3>=0){
+           if(sum1==sum2 && sum2==sum3) return sum1;
+           if(sum1>sum2) sum1-=s1[n1--];
+           if(sum2>sum3) sum2-=s2[n2--];
+           if(sum3>sum1) sum3-=s3[n3--];
+       }
+       return 0;
+   }
