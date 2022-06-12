80eb3ee68c731a2512b947dc374b38571eca26c0
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-12 17:03:31 +0530

    revise frequently

---
+++ 08_Greedy/31_Min_Cost_of_Ropes.cpp
@@ -0,0 +1,24 @@
+// Easy solution. we are first sorting the array and then finding the minimum cost of adding joining the ropes going from lower values to higher values.
+
+class Solution{
+  public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for(long long i=0; i<n; i++){
+            pq.push(arr[i]);
+        }for
+        
+        long long  ans = 0;
+        while(pq.size() > 1){
+            long long a = pq.top(); pq.pop();
+            long long b = pq.top(); pq.pop();
+            long long sum = a + b;
+            ans += sum;
+            pq.push(sum);
+        }
+        
+        return ans;
+    }
+};
