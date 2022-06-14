cb4de891db962b21e2b1066e124eae32b2c8d34b
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-14 08:55:51 +0530

    revise

---
+++ 14_Dynamic_Programming/Extra_03_Delete_Operation_for_2_strings.cpp
@@ -0,0 +1,32 @@
+// This is the DP based solution solved by me.
+// Hence it is concluded that revision is important.
+
+
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        int n = s1.length();  int m = s2.length();
+        int dp[n+1][m+1];
+        
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i;
+        }
+        
+        for(int j=0; j<=m; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1])+1;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
