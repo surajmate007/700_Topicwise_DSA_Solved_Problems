e0580c84e4370a76f3834520446e662e1194c8df
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-15 14:58:56 +0530

    revise

---
+++ 09_Backtracking/Extra_00_Longest_Route_from_Source_to_Dest.cpp
@@ -0,0 +1,39 @@
+// Solved using backtracking.
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void getAns(int n, int m, vector<vector<int>>& a, int sx, int sy, int dx, int dy, int d, int& ans){
+    if(sx == dx and sy == dy){
+        ans = max(ans, d);
+        return;
+    }
+    
+    a[sx][sy] = 0;
+    if(sx-1>=0 and a[sx-1][sy]==1){
+        getAns(n, m, a, sx-1, sy, dx, dy, d+1, ans);
+    }
+    if(sy-1>=0 and a[sx][sy-1]==1){
+        getAns(n, m, a, sx, sy-1, dx, dy, d+1, ans);
+    }
+    if(sx+1<n and a[sx+1][sy]==1){
+        getAns(n, m, a, sx+1, sy, dx, dy, d+1, ans);
+    }
+    if(sy+1<m and a[sx][sy+1]==1){
+        getAns(n, m, a, sx, sy+1, dx, dy, d+1, ans);
+    }
+    a[sx][sy] = 1;
+}
+
+
+int longestPath(int n, int m, vector<vector<int>> &mat, int sx, int sy, int dx, int dy) {
+    if(mat[sx][sy] == 0 or mat[dx][dy] == 0){
+        return -1;
+    }
+    
+    int ans = -1;
+	getAns(n, m, mat, sx, sy, dx, dy, 0, ans);
+    
+    return ans;
+}
