491b2032307344e085ebe2f33e2832e4d1b34410
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-17 11:40:19 +0530

    revise frequently

--- 14_Dynamic_Programming/42_Longest_Palindromic_Substring.cpp
+++ 14_Dynamic_Programming/42_Longest_Palindromic_Substring.cpp
@@ -51,3 +51,42 @@ int longestPalSubstr(string str){
             }
         }
     }
+    
+    
+    
+class Solution {
+    public:
+    
+    string longestPalindrome(string s) {
+        int n = s.length(); int maxlen = 1; int start = 0;
+        int dp[n][n];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i=0; i<n; i++){
+            dp[i][i] = 1;
+        }
+        
+        for(int i=0; i<n-1; i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = 1;
+                maxlen = 2; start = i;
+            }
+        }
+        
+        for(int k=3; k<=n; k++){
+            for(int i=0; i<n-k+1; i++){
+                int j = i+k-1;
+                if(dp[i+1][j-1] and s[i] == s[j]){
+                    dp[i][j] = 1;
+                    
+                    if(k > maxlen){
+                        maxlen = k; start = i;
+                    }
+                }
+            }
+        }
+        
+        string ans = s.substr(start, maxlen);
+        return ans;
+    }
+};
