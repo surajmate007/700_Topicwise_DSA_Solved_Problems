df4957104887492f8cf50b214c2793b17af5a7fc
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-18 09:30:37 +0530

    revise

---
+++ 03_String/Extra_00_prefix_and_suffix_search.cpp
@@ -0,0 +1,28 @@
+// The solution approach is simple. we are first storing all the possible combinations of prefix and suffix into the map and then
+// searching for the word based on it.
+
+class WordFilter {
+    unordered_map<string,int> data;
+public:
+    WordFilter(vector<string>& words) {
+        for (int k = 0; k < words.size(); ++k) {
+            string const& word = words[k];
+            int const n = word.size();
+            for (int i = 1; i <= n; ++i) {
+                string key = word.substr(0, i);
+                key += '.';
+                for (int j = 1; j <= n; ++j) {
+                    key.resize(i+1);
+                    key.append(word, n-j, j);
+                    data[key] = k;
+                }
+            }
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        prefix += '.';
+        prefix += suffix;
+        return data.count(prefix) ? data[prefix] : -1;
+    }
+};
