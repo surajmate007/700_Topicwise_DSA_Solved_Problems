dbfdc60975206528095dc4d8a6f7616a0b1c1b85
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-21 16:46:35 +0530

    revise frequently.

---
+++ 11_Heap/01_Heap_Sort.cpp
@@ -0,0 +1,89 @@
+// Heap sort is a comparison-based sorting technique based on Binary Heap data structure. It is similar to selection sort where we first find the minimum
+// element and place the minimum element at the beginning. We repeat the same process for the remaining elements.
+
+// Let us first define a Complete Binary Tree. A complete binary tree is a binary tree in which every level, except possibly the last, is completely
+// filled, and all nodes are as far left as possible (Source Wikipedia)
+// A Binary Heap is a Complete Binary Tree where items are stored in a special order such that the value in a parent node is greater(or smaller)
+// than the values in its two children nodes. The former is called max heap and the latter is called min-heap. The heap can be represented by a binary
+// tree or array.
+
+
+// C++ program for implementation of Heap Sort
+#include <iostream>
+
+using namespace std;
+
+// To heapify a subtree rooted with node i
+// which is an index in arr[].
+// n is size of heap
+void heapify(int arr[], int n, int i)
+{
+	
+// Initialize largest as root
+	int largest = i;
+	
+// left = 2*i + 1
+	int l = 2 * i + 1;
+	
+// right = 2*i + 2
+	int r = 2 * i + 2;
+
+	// If left child is larger than root
+	if (l < n && arr[l] > arr[largest])
+		largest = l;
+
+	// If right child is larger than largest
+	// so far
+	if (r < n && arr[r] > arr[largest])
+		largest = r;
+
+	// If largest is not root
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+
+		// Recursively heapify the affected
+		// sub-tree
+		heapify(arr, n, largest);
+	}
+}
+
+// Main function to do heap sort
+void heapSort(int arr[], int n)
+{
+	
+	// Build heap (rearrange array)
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+
+	// One by one extract an element
+	// from heap
+	for (int i = n - 1; i > 0; i--) {
+		
+		// Move current root to end
+		swap(arr[0], arr[i]);
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0);
+	}
+}
+
+// A utility function to print array of size n
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	heapSort(arr, n);
+
+	cout << "Sorted array is \n";
+	printArray(arr, n);
+}
+
