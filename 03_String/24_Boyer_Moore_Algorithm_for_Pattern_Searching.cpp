c5f491eaf4e2a344afe0fb9994c98249fa89a83a
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-21 10:43:07 +0530

    read frequently

--- 03_String/24_Boyer_Moore_Algorithm_for_Pattern_Searching.cpp
+++ 03_String/24_Boyer_Moore_Algorithm_for_Pattern_Searching.cpp
@@ -3,6 +3,54 @@
 // very well explained. Too intuitive algorithm. Liked it.
 
 
+// Pattern searching is an important problem in computer science. When we do search for a string in a notepad/word file, browser, or database, pattern
+// searching algorithms are used to show the search results. A typical problem statement would be- 
+// Given a text txt[0..n-1] and a pattern pat[0..m-1] where n is the length of the text and m is the length of the pattern, write a function
+// search(char pat[], char txt[]) that prints all occurrences of pat[] in txt[]. You may assume that n > m. 
+
+// In this post, we will discuss the Boyer Moore pattern searching algorithm. Like KMP and Finite Automata algorithms, Boyer Moore algorithm
+// also preprocesses the pattern.
+	
+// Boyer Moore is a combination of the following two approaches. 
+// Bad Character Heuristic 
+// Good Suffix Heuristic 
+
+// Both of the above heuristics can also be used independently to search a pattern in a text. Let us first understand how two independent approaches
+// work together in the Boyer Moore algorithm. If we take a look at the Naive algorithm, it slides the pattern over the text one by one. KMP algorithm
+// does preprocessing over the pattern so that the pattern can be shifted by more than one. The Boyer Moore algorithm does preprocessing for the same reason.
+// It processes the pattern and creates different arrays for each of the two heuristics. At every step, it slides the pattern by the max of the slides
+// suggested by each of the two heuristics. So it uses greatest offset suggested by the two heuristics at every step. 
+
+// Unlike the previous pattern searching algorithms, the Boyer Moore algorithm starts matching from the last character of the pattern.
+// In this post, we will discuss the bad character heuristic and the Good Suffix heuristic in the next post. 
+
+// Bad Character Heuristic
+
+// The idea of bad character heuristic is simple. The character of the text which doesn’t match with the current character of the pattern is called
+// the Bad Character. Upon mismatch, we shift the pattern until the mismatch becomes a match Pattern P moves past the mismatched character.
+	
+// Case 1 – Mismatch become match 
+// We will lookup the position of the last occurrence of the mismatched character in the pattern, and if the mismatched character exists in the pattern,
+// then we’ll shift the pattern such that it becomes aligned to the mismatched character in the text T. 
+// Explanation: In the above example, we got a mismatch at position 3. Here our mismatching character is “A”. Now we will search for last occurrence of
+// “A” in pattern. We got “A” at position 1 in pattern (displayed in Blue) and this is the last occurrence of it. Now we will shift pattern 2 times so
+// that “A” in pattern get aligned with “A” in text.
+
+// Case 2 – Pattern move past the mismatch character 
+// We’ll lookup the position of last occurrence of mismatching character in pattern and if character does not exist we will shift pattern past the
+// mismatching character. 
+
+// Explanation: 
+
+// Here we have a mismatch at position 7. The mismatching character “C” does not exist in pattern before position 7 so we’ll shift pattern past to the
+// position 7 and eventually in above example we have got a perfect match of pattern (displayed in Green). We are doing this because “C” does not exist
+// in the pattern so at every shift before position 7 we will get mismatch and our search will be fruitless.
+
+// In the following implementation, we preprocess the pattern and store the last occurrence of every possible character in an array of size equal to
+// alphabet size. If the character is not present at all, then it may result in a shift by m (length of pattern). Therefore, the bad character heuristic
+// takes O(n/m) time in the best case. 
+
+
 /* C++ Program for Bad Character Heuristic of Boyer
 Moore String Matching Algorithm */
 #include <bits/stdc++.h>
