c89ff8cd8b3aa05f1013634a070767a58fcb6100
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-21 20:47:12 +0530

    revise frequently

--- 11_Heap/06_Merge_2_Binary_Max_Heaps.cpp
+++ 11_Heap/06_Merge_2_Binary_Max_Heaps.cpp
@@ -1,1 +1,40 @@
+// This is the simple solution where the heaps are given in array format and then we are merging the arrays and applying heapifi on them.
 
+class Solution{
+    public:
+    void heapify(vector<int>& arr, int n, int i){
+        int largest = i;
+        int l = 2*i+1;
+        int r = 2*i+2;
+        
+        if(l < n and arr[l] > arr[i]){
+            largest = l;
+        }
+        
+        if(r < n and arr[r] > arr[largest]){
+            largest = r;
+        }
+        
+        if(i != largest){
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+    
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        vector<int> ans;
+        for(int i=0; i<n; i++){
+            ans.push_back(a[i]);
+        }
+        for(int i=0; i<m; i++){
+            ans.push_back(b[i]);
+        }
+        
+        n = n+m;
+        for(int i=(n/2)-1; i>=0; i--){
+            heapify(ans, n, i);
+        }
+        
+        return ans;
+    }
+};
