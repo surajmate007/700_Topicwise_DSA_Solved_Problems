a31ee0ad7a7a281cafc4137c8d0f098904041d0c
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-27 21:44:04 +0530

    revise frequently.

---
+++ 12_Graph/Extra_01_Check_if_a_Graph_is_bipartatite_BFS_DFS.cpp
@@ -0,0 +1,109 @@
+// usig BFS:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool bipartiteBfs(int src, vector<int> adj[], int color[]) {
+    queue<int>q;
+    q.push(src); 
+    color[src] = 1; 
+    while(!q.empty()) {
+        int node = q.front(); 
+        q.pop();
+        
+        for(auto it : adj[node]) {
+            if(color[it] == -1) {
+                color[it] = 1 - color[node]; 
+                q.push(it); 
+            } else if(color[it] == color[node]) {
+                return false; 
+            }
+        }
+    }
+    return true; 
+}
+
+bool checkBipartite(vector<int> adj[], int n) {
+    int color[n];
+    memset(color, -1, sizeof color); 
+    for(int i = 0;i<n;i++) {
+        if(color[i] == -1) {
+            if(!bipartiteBfs(i, adj, color)) {
+                return false;
+            }
+        }
+    }
+    return true; 
+}
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> adj[n];
+	for(int i = 0;i<m;i++) {
+	    int u, v;
+	    cin >> u >> v;
+	    adj[u].push_back(v); 
+	    adj[v].push_back(u); 
+	}
+	
+	if(checkBipartite(adj, n)) {
+	    cout << "yes"; 
+	} else {
+	    cout << "No"; 
+	}
+	return 0;
+}
+
+
+// USING DFS:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool bipartiteDfs(int node, vector<int> adj[], int color[]) {
+    for(auto it : adj[node]) {
+        if(color[it] == -1) {
+            color[it] = 1 - color[node];
+            if(!bipartiteDfs(it, adj, color)) {
+                return false; 
+            }
+        } else if(color[it] == color[node]) return false; 
+    }
+    return true; 
+}
+bool checkBipartite(vector<int> adj[], int n) {
+    int color[n];
+    memset(color, -1, sizeof color); 
+    for(int i = 0;i<n;i++) {
+        if(color[i] == -1) {
+            color[i] = 1;
+            if(!bipartiteDfs(i, adj, color)) {
+                return false;
+            }
+        } 
+    }
+    return true; 
+}
+void addedge(vector<int> adj[],int u,int v)
+{
+        adj[u].push_back(v); 
+        adj[v].push_back(u);
+}
+int main() {
+    
+    vector<int> adj[6];
+    addedge(adj,0,1);
+    addedge(adj,1,2);
+    addedge(adj,1,4);
+    addedge(adj,1,5);
+    addedge(adj,2,3);
+    addedge(adj,3,4);
+    addedge(adj,3,5);
+
+    if(checkBipartite(adj, 6)) {
+        cout << "It is a Bipartite Graph"; 
+    } else {
+        cout << "It is not a Bipartite Graph"; 
+    }
+    return 0;
+}
