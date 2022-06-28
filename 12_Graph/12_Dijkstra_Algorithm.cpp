45b3b20afe03aefb0b145228ef974ee673e064a7
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-06-28 15:03:40 +0530

    revise frequently.

--- 12_Graph/12_Dijkstra_Algorithm.cpp
+++ 12_Graph/12_Dijkstra_Algorithm.cpp
@@ -3,26 +3,26 @@
 // Thus this is single source shortest path problem.
 
 
- // } Driver Code Ends
 class Solution{
-	public:
+    public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
     
     vector <int> dijkstra(int n, vector<vector<int>> adj[], int s){
         vector<int> dist(n, 1e9);
-        queue<int> q;
-        q.push(s);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > q;
         dist[s] = 0;
+        q.push({dist[s], s});
         
         while(!q.empty()){
-            int e = q.front();
+            auto pr = q.top();
+            int e = pr.second; int d = pr.first;
             q.pop();
             for(auto k : adj[e]){
                 int x = k[0]; int y = k[1];
-                if(dist[x] > dist[e] + y){
-                    dist[x] = dist[e] + y;
-                    q.push(x);
+                if(dist[x] > d + y){
+                    dist[x] = d + y;
+                    q.push({dist[x], x});
                 }
             }
         }
