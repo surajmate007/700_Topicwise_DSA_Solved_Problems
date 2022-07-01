72e28fe39270bd071389e3596b1645e988c5fb5a
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-01 10:55:28 +0530

    revise frequently.

---
+++ 12_Graph/06_Search_in_a_Maze.cpp
@@ -0,0 +1,40 @@
+// Rat in a maze problem as it is. 
+
+class Solution{
+    public:
+    void getPath(vector<vector<int>>& m, int n, int i, int j, string ds, vector<string>& ans){
+        if(i<0 or j<0 or i>=n or j>=n){
+            return;
+        }
+        
+        if(m[i][j] == 0){
+            return;
+        }
+        
+        if(i==n-1 and j==n-1){
+            ans.push_back(ds);
+            return;
+        }
+        
+        m[i][j] = 0;
+        
+        getPath(m, n, i+1, j, ds+'D', ans);
+        getPath(m, n, i, j+1, ds+'R', ans);
+        getPath(m, n, i-1, j, ds+'U', ans);
+        getPath(m, n, i, j-1, ds+'L', ans);
+        
+        m[i][j] = 1;
+    }
+    
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        
+        if(m[0][0] == 0){
+            return ans;
+        }
+        
+        getPath(m, n, 0, 0, "", ans);
+        
+        return ans;
+    }
+};
