fa3841f81e640834a0263c97ca34c7fc8b67d31c
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-06 14:37:14 +0530

    revise

---
+++ 14_Dynamic_Programming/25_Max_Sum_of_Pairs_with_Specific_Difference.cpp
@@ -0,0 +1,18 @@
+// this is the simple solution where we are first sorting the array and then starting from the end of the array and keep on forming the pairs greedily.
+
+class Solution{
+    public:
+    int maxSumPairWithDifferenceLessThanK(int arr[], int n, int k){
+        sort(arr, arr+n);
+        int ans = 0;
+        
+        for(int i=n-1; i>0; i--){
+            if(arr[i]-arr[i-1] < k){
+                ans += (arr[i] + arr[i-1]);
+                i--;
+            }
+        }
+        
+        return
+    }
+};
