4f4953d0fe88869de6174e51efb01e571811db03
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-08 10:52:23 +0530

    watch striver dp series

---
+++ 14_Dynamic_Programming/57_Max_Profit_of_Buying_and_Selling_Stock_at_most_k_times.cpp
@@ -0,0 +1,38 @@
+ class Solution {
+  public:
+  
+    int maxProfit(int k, int n, int price[]) {
+
+    // table to store results of subproblems
+    // profit[t][i] stores maximum profit using
+    // atmost t transactions up to day i (including
+    // day i)
+    int profit[k + 1][n + 1];
+
+    // For day 0, you can't earn money
+    // irrespective of how many times you trade
+    for (int i = 0; i <= k; i++)
+        profit[i][0] = 0;
+
+    // profit is 0 if we don't do any transaction
+    // (i.e. k =0)
+    for (int j = 0; j <= n; j++)
+        profit[0][j] = 0;
+
+    // fill the table in bottom-up fashion
+    for (int i = 1; i <= k; i++) {
+        for (int j = 1; j < n; j++) {
+            int max_so_far = INT_MIN;
+
+            for (int m = 0; m < j; m++)
+                max_so_far = max(max_so_far,
+                                 price[j] - price[m] + profit[i - 1][m]);
+
+            profit[i][j] = max(profit[i][j - 1], max_so_far);
+        }
+    }
+
+    return profit[k][n - 1];
+}
+
+};
