0e82d48c62c2f10236f741c1030b81fc9a62b81e
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-09 10:52:50 +0530

    revise

--- 01_Arrays/14_Next_Permutation.cpp
+++ 01_Arrays/14_Next_Permutation.cpp
@@ -7,7 +7,7 @@ public:
     void nextPermutation(vector<int>& nums) {
         int idx1 = -1; int idx2 = -1; int n = nums.size()-1;
         
-        for(int i=n-1; i>=0; i--){
+        for(int i=n-2; i>=0; i--){
             if(nums[i] < nums[i+1]){
                 idx1 = i;
                 break;
@@ -15,7 +15,7 @@ public:
         }
         
         if(idx1 == -1){
-            reverse(nums.begin() + idx1 + 1, nums.end());
+            reverse(nums.begin(), nums.end());
             return;
         }
         
