43339f3ceb90422dbdf5f2218616ebd3c820edd0
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-11 10:34:47 +0530

    revise

--- 03_String/20_Min_Number_of_bracket_reversals.cpp
+++ 03_String/20_Min_Number_of_bracket_reversals.cpp
@@ -30,7 +30,7 @@ int countRev (string s){
 // Better algo for this :
 
 int minRemovals(string s, int n){
-        int open = 0; int close = 0; stack<char> st;
+        stack<char> st;
         for(int i=0; i<n; i++){
             if(s[i] == '('){
                 st.push(s[i]);
