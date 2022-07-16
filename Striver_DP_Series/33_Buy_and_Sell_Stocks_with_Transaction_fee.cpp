988dba7a07195d063d46859cc738a21db259b369
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-16 10:39:05 +0530

    revise

---
+++ Striver_DP_Series/33_Buy_and_Sell_Stocks_with_Transaction_fee.cpp
@@ -0,0 +1,72 @@
+// Recursion:
+
+
+class Solution {
+public:
+    
+    int getAns(vector<int>& prices, int n, int fee, int buy, int idx){
+        if(idx >= n){
+            return 0;
+        }
+        
+        int ans = 0;
+        
+        if(buy == 1){
+            int ans1 = -prices[idx] + getAns(prices, n, fee, 0, idx+1);
+            int ans2 = getAns(prices, n, fee, 1, idx+1);
+            ans = max(ans1, ans2);
+        }
+        else{
+            int ans1 = -fee + prices[idx] + getAns(prices, n, fee, 1, idx+1);
+            int ans2 = getAns(prices, n, fee, 0, idx+1);
+            ans = max(ans1, ans2);
+        }
+        
+        return ans;
+    }
+    
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        return getAns(prices, n, fee, 1, 0);
+    }
+};
+
+
+
+// Memoisation:
+
+
+class Solution {
+public:
+    
+    int getAns(vector<int>& prices, int n, int fee, int buy, int idx, vector<vector<int>>& dp){
+        if(idx >= n){
+            return 0;
+        }
+        
+        if(dp[idx][buy] != -1){
+            return dp[idx][buy];
+        }
+        
+        int ans = 0;
+        
+        if(buy == 1){
+            int ans1 = -prices[idx] + getAns(prices, n, fee, 0, idx+1, dp);
+            int ans2 = getAns(prices, n, fee, 1, idx+1, dp);
+            ans = max(ans1, ans2);
+        }
+        else{
+            int ans1 = -fee + prices[idx] + getAns(prices, n, fee, 1, idx+1, dp);
+            int ans2 = getAns(prices, n, fee, 0, idx+1, dp);
+            ans = max(ans1, ans2);
+        }
+        
+        return dp[idx][buy] = ans;
+    }
+    
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int> (2, -1));
+        return getAns(prices, n, fee, 1, 0, dp);
+    }
+};
