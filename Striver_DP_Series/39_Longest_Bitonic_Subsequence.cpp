8211c37cd803a30c520c641af292c98aab57f9a8
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-20 16:33:27 +0530

    revise

--- Striver_DP_Series/39_Longest_Bitonic_Subsequence.cpp
+++ Striver_DP_Series/39_Longest_Bitonic_Subsequence.cpp
@@ -29,3 +29,42 @@ int longestBitonicSequence(vector<int>& arr, int n) {
     return ans;
 }
 
+
+
+// Similar Approach of longets bitonic subsequence:
+// Longest mountain in array:
+
+
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        vector<int> narr = arr;
+        reverse(narr.begin(), narr.end());
+        
+        int n = arr.size();
+        
+        vector<int> lis (n, 1), lds(n, 1);
+        
+        for(int i=1; i<n; i++){
+            if(arr[i] > arr[i-1]){
+                lis[i] = lis[i-1]+1;
+            }
+            if(narr[i] > narr[i-1]){
+                lds[i] = lds[i-1]+1;
+            }
+        }
+        
+        reverse(lds.begin(), lds.end());
+        
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(lis[i] != 1 and lds[i] != 1){
+                ans = max(ans, lis[i]+lds[i]-1);
+            }  
+        }
+        
+        return ans;
+    }
+};
+
+
