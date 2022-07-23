64a5b041328eb70e734f6d706242d7ad40c59884
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-23 14:45:47 +0530

    revise

--- 09_Backtracking/Extra_01_Matchsticks_to_Squares.cpp
+++ 09_Backtracking/Extra_01_Matchsticks_to_Squares.cpp
@@ -1,35 +1,42 @@
 // Backtracking Solution:
 
-class Solution{
-  public:
-  
-  bool dfs(vector<int>& matchsticks, vector<int>& kow, int index, int target){
-          if(index == -1){
-              return true;
-          }
-          for(int i = 0; i < 4; i++){
-              if(((kow[i] + matchsticks[index]) > target) or (i > 0 and kow[i] == kow[i - 1])){
-                  continue;
-              }
-              kow[i] += matchsticks[index];
-              if(dfs(matchsticks, kow, index - 1, target)){
-                  return true;
-              } 
-              kow[i] -= matchsticks[index];
-          }
-          return false;
-      }
+class Solution {
+    public:
+    
+    bool getAns(vector<int>& ms, int n, vector<int>& used, int osum, int csum, int idx, int k){
+        if(k==1){
+            return true;
+        }
+        
+        if(csum == osum){
+            return getAns(ms, n, used, osum, 0, 0, k-1);
+        }
+        
+        for(int i=idx; i<n; i++){
+            if(used[i] == 0 and csum+ms[i] <= osum){
+                used[i] = 1;
+                if(getAns(ms, n, used, osum, csum+ms[i], i+1, k) == true){
+                    return true;
+                }
+                used[i] = 0;
+            }
+        }
+        
+        return false;
+    }
+    
+    bool makesquare(vector<int>& ms){
+        int n = ms.size();
+        int osum = accumulate(ms.begin(), ms.end(), 0);
+        if(osum%4 != 0 or ms.size() < 3){
+            return false;
+        }
+        osum /= 4;
+        
+        sort(ms.begin(), ms.end(), greater<int>());
+        
+        vector<int> used(n, 0);
 
-      bool makesquare(vector<int>& matchsticks) {
-          int sum = 0;
-          for(int i : matchsticks){
-              sum += i;
-          }
-          if(sum%4 != 0 or matchsticks.size() < 3){
-              return false;
-          }
-          sort(matchsticks.begin(), matchsticks.end());
-          vector<int> kow(4, 0);
-          return dfs(matchsticks, kow, matchsticks.size() - 1, sum/4);
-      }
+        return getAns(ms, n, used, osum, 0, 0, 4);
+    }
 };
