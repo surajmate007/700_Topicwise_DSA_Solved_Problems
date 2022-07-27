738709e59bc2a9ed3f67e7d4673546c4585d1dfb
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-27 17:31:13 +0530

    revise

---
+++ 16_Binary_Search/07_Seach_In_Rotated_Sorted_Array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lo = 0; int hi = n-1;
+        while(lo <= hi){
+            int mid = (lo+hi)>>1;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] >= nums[lo]){
+                if(target>=nums[lo] and target <= nums[mid]){
+                    hi = mid-1;
+                }
+                else{
+                    lo = mid+1;
+                }
+            }
+            else{
+                if(target<=nums[hi] and target >= nums[mid]){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid-1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
