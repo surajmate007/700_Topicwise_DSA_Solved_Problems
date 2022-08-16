554154fbb48149b3364433c1b470de4d6a1f71d6
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-08-16 10:18:07 +0530

    revise

--- 11_Heap/07_K-th_Largest_Sum_Contiguous_Subarray.cpp
+++ 11_Heap/07_K-th_Largest_Sum_Contiguous_Subarray.cpp
@@ -9,27 +9,20 @@
 
 class Solution{
 public:
-    int kthLargest(vector<int> &a,int n,int k){
-        int sum [n+1];
-        sum[0] = 0; sum[1] = a[0];
-        
-        for(int i=2; i<=n; i++){
-            sum[i] = sum[i-1] + a[i-1];
+    int kthLargest(vector<int> &arr,int n,int k){
+        vector<int> sum(n+1);
+        sum[0] = 0; sum[1] = arr[0];
+        for(int i=1; i<n; i++){
+            sum[i+1] = sum[i] + arr[i];
         }
-        priority_queue<int, vector<int>, greater<int>> pq;
         
+        priority_queue<int, vector<int>, greater<int>> pq;
         for(int i=1; i<=n; i++){
             for(int j=i; j<=n; j++){
-                int x = sum[j] - sum[i-1];
-                
-                if(pq.size() < k){
-                    pq.push(x);
-                }
-                else{
-                    if(pq.top() < x){
-                        pq.pop();
-                        pq.push(x);
-                    }
+                int val = sum[j] - sum[i-1];
+                pq.push(val);
+                if(pq.size() > k){
+                    pq.pop();
                 }
             }
         }
