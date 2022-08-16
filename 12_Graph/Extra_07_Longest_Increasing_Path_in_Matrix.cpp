c6b0b2947395b3084c6664bd34266035c7f8330b
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-08-16 20:29:41 +0530

    revise

--- 12_Graph/Extra_07_Longest_Increasing_Path_in_Matrix.cpp
+++ 12_Graph/Extra_07_Longest_Increasing_Path_in_Matrix.cpp
@@ -1,3 +1,45 @@
+// I was too uch surprised when this question was solved by sarang in the contest🙁😳🤯
+
+// Now my solution accepted in one go:
+
+class Solution {
+public:
+    int dir[5] = {0, 1, 0, -1, 0};
+    
+    int getAns(vector<vector<int>>& matrix, int i, int j, int n, int m, vector<vector<int>>& dp){
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int ans = 1;
+        for(int k=0; k<4; k++){
+            int x = i+dir[k]; int y = j+dir[k+1];
+            if(x>=0 and y>=0 and x<n and y<m and matrix[x][y] > matrix[i][j]){
+                ans = max(ans, 1 + getAns(matrix, x, y, n, m, dp));
+            }
+        }
+        
+        return dp[i][j] = ans;
+    }
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int ans = 0;
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                ans = max(ans, getAns(matrix, i, j, n, m, dp));
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
 // DP + DFS soution.
 
 class Solution {
