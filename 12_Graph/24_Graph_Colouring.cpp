e639b887275f34ee854f3b8f10be0372db4d540d
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-08-16 16:42:38 +0530

    revise

--- 12_Graph/24_Graph_Colouring.cpp
+++ 12_Graph/24_Graph_Colouring.cpp
@@ -31,3 +31,47 @@
 // 6) Map Coloring: Geographical maps of countries or states where no two adjacent cities cannot be assigned same color. Four colors are sufficient to
 // color any map (See Four Color Theorem) 
 
+
+// Here we are trying every colour for every single node and checking whether the colouring is possible or not.
+// If not then we are backtracing 
+// Vector color store the mapping of colours corresponding to vertices.
+
+// Function to determine if graph can be coloured with at most M colours such
+// that no two adjacent vertices of graph are coloured with same colour.
+
+bool isPossible(int i, int c, bool graph[101][101], vector<int> color){
+    for(int k = 0; k<101; k++){
+        if(graph[i][k] == 1){
+            if(color[k] == c){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+bool getAns(bool graph[101][101], int m, int n, vector<int>& color){
+    for(int i=0; i<n; i++){
+        if(color[i] == 0){
+            for(int c=1; c<=m; c++){
+                if(isPossible(i, c, graph, color)){
+                    color[i] = c;
+                    
+                    if(getAns(graph, m, n, color) == true){
+                        return true;
+                    }
+                    
+                    color[i] = 0;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+bool graphColoring(bool graph[101][101], int m, int n) {
+    vector<int> color(n, 0);
+
+    return getAns(graph, m, n, color);
+}
