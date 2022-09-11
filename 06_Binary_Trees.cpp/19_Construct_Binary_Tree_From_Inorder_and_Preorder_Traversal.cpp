8c2be9b5e33d910b0424945df696afedbecc2c96
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-09-11 20:48:52 +0530

    revise

--- 06_Binary_Trees.cpp/19_Construct_Binary_Tree_From_Inorder_and_Preorder_Traversal.cpp
+++ 06_Binary_Trees.cpp/19_Construct_Binary_Tree_From_Inorder_and_Preorder_Traversal.cpp
@@ -1,38 +1,36 @@
 // we are simply using divide and conquer approach.
 // understood from this video : https: //www.youtube.com/watch?v=aZNaLrVebKQ
 
-// Coded by me
+// Coded by me.
+// More optimal than previous no for loop.
 
-class Solution{
-    public:
+class Solution {
+public:
     unordered_map<int, int> mp;
     int idx = 0;
     
-    Node* construct(int in[], int i, int j){
-        
-        if(i>j){
+    TreeNode* formTree(vector<int>& pre, int lo, int hi){
+        if(lo > hi){
             return NULL;
         }
         
-        Node* root;
-        for(int k=i; k<=j; k++){
-            if(mp[in[k]] == idx){
-                root = new Node(in[k]);
-                idx++;
-                root->left = construct(in, i, k-1);
-                root->right = construct(in, k+1, j);
-            }
-        }
+        int p = mp[pre[idx]];
+        TreeNode* nd = new TreeNode(pre[idx]);
         
-        return root; 
+        idx++;
+        nd->left = formTree(pre, lo, p-1);
+        nd->right = formTree(pre, p+1, hi);
+        
+        return nd;
     }
     
-    Node* buildTree(int in[],int pre[], int n){
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        mp.clear(); idx = 0;
+        int n = inorder.size();
         for(int i=0; i<n; i++){
-            mp[pre[i]] = i;
+            mp[inorder[i]] = i;
         }
         
-        Node* head = construct(in, 0, n-1);
-        return head;
+        return formTree(preorder, 0, n-1);
     }
 };
