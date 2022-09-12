57ef6fd71a9ff7427b0f012f1c7d8e17e3dc54f0
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-09-12 21:11:47 +0530

    revise

--- 06_Binary_Trees.cpp/Extra_00_Serialixe_and_Deserialize_BST.cpp
+++ 06_Binary_Trees.cpp/Extra_00_Serialixe_and_Deserialize_BST.cpp
@@ -1,3 +1,85 @@
+// Better solution : 
+
+
+#include<bits/stdc++.h>
+
+
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+string serializeTree(TreeNode<int>* root){
+    if(!root) return "";
+        
+        string s ="";
+        queue<TreeNode<int>*> q;
+        q.push(root);
+        while(!q.empty()) {
+           TreeNode<int>* curNode = q.front();
+           q.pop();
+           if(curNode==NULL) s.append("#,");
+           else s.append(to_string(curNode->data)+',');
+           if(curNode != NULL){
+               q.push(curNode->left);
+               q.push(curNode->right);            
+           }
+        }
+        return s;
+}
+
+TreeNode<int>* deserializeTree(string& data){
+    if(data.size() == 0) return NULL; 
+        stringstream s(data);
+        string str; 
+        getline(s, str, ',');
+        TreeNode<int> *root = new TreeNode<int>(stoi(str));
+        queue<TreeNode<int>*> q; 
+        q.push(root); 
+        while(!q.empty()) {
+            
+            TreeNode<int> *node = q.front(); 
+            q.pop(); 
+            
+            getline(s, str, ',');
+            if(str == "#") {
+                node->left = NULL; 
+            }
+            else {
+                TreeNode<int>* leftNode = new TreeNode<int>(stoi(str)); 
+                node->left = leftNode; 
+                q.push(leftNode); 
+            }
+            
+            getline(s, str, ',');
+            if(str == "#") {
+                node->right = NULL; 
+            }
+            else {
+                TreeNode<int>* rightNode = new TreeNode<int>(stoi(str)); 
+                node->right = rightNode;
+                q.push(rightNode); 
+            }
+        }
+        return root; 
+}
+
+
 // simple approach of finding the string from BST and then converting this string into the BST. from preorder and inorder traversal.
 
 class Codec {
