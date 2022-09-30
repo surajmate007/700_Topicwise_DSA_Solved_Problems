8adf08e643b44c5dc488fd0917e13534e0837fe5
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-09-30 17:43:35 +0530

    revise frequently

--- 12_Graph/12_Dijkstra_Algorithm.cpp
+++ 12_Graph/12_Dijkstra_Algorithm.cpp
@@ -50,19 +50,19 @@ class Solution{
     
     vector <int> dijkstra(int n, vector<vector<int>> adj[], int s){
         vector<int> dist(n, 1e9);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > q;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
         dist[s] = 0;
-        q.push({dist[s], s});
+        pq.push({dist[s], s});
         
-        while(!q.empty()){
-            auto pr = q.top();
+        while(!pq.empty()){
+            auto pr = pq.top();
             int e = pr.second; int d = pr.first;
-            q.pop();
+            pq.pop();
             for(auto k : adj[e]){
                 int x = k[0]; int y = k[1];
                 if(dist[x] > d + y){
                     dist[x] = d + y;
-                    q.push({dist[x], x});
+                    pq.push({dist[x], x});
                 }
             }
         }
