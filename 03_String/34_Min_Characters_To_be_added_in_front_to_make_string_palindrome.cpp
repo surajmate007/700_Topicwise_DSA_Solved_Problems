e6d2e904c02224dd025c59c43a9ed3d3e69b868f
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-10-04 11:01:18 +0530

    revise

--- 03_String/34_Min_Characters_To_be_added_in_front_to_make_string_palindrome.cpp
+++ 03_String/34_Min_Characters_To_be_added_in_front_to_make_string_palindrome.cpp
@@ -2,7 +2,48 @@
 // 1. we will first compute the length of max palindromic substring starting from 0th index.
 // 2. Then return all the remaining characters which are not part of this substring.
 
+// O(n) Approach:
+// Today I understood the use of longest prefx and suffix (lps):
 
+class Solution {
+public:
+
+    void findLPS(string str, int n, vector<int>& lps){
+        int i=1; int j=0;
+        
+        while(i<n){
+            if(str[i] == str[j]){
+                lps[i] = j+1;
+                i++; j++;
+            }
+            else{
+                if(j==0){
+                    i++;
+                }
+                else{
+                    j = lps[j-1];
+                }
+            }
+        }
+    }
+
+    int minChar(string s){
+        int n = s.length();
+        string p = s;
+        reverse(p.begin(), p.end());
+        string str = s + '&' + p;
+        int len = str.length();
+        vector<int> lps(len, 0);
+        findLPS(str, len, lps);
+        return n - lps.back();
+    }
+};
+
+
+
+
+
+// TC : O(n^2)
 bool isPalin(string s){
         int i=0; int j=s.length()-1;
         while(i<j){
