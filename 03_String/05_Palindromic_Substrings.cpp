76fbc10cf94867b5c297f3e82800dfe1373265f3
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-10-29 18:05:01 +0530

    revsie

---
+++ 03_String/05_Palindromic_Substrings.cpp
@@ -0,0 +1,29 @@
+// siple code but I am not able top write the recursion for this code. 😢🥺
+
+class Solution {
+public:
+    int isPalin(string& s, int start, int end){
+        while(start < end){
+            if(s[start] != s[end]){
+                return 0;
+            }
+            start++; end--;
+        }
+        return 1;
+    }
+
+    int getAns(string& s, int n){
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                ans += isPalin(s, i, j);
+            }
+        }
+        return ans;
+    }
+
+    int countSubstrings(string s) {
+        int n = s.length();
+        return getAns(s, n);
+    }
+};
