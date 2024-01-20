b2a9d16664d43fdb35f41968c1f0e8bccfaa084f
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2024-01-20 17:42:29 +0530

    Update 30_Smallest_Subarray_with_sum_greater_than_a_given_value.cpp
    good solution

--- 01_Arrays/30_Smallest_Subarray_with_sum_greater_than_a_given_value.cpp
+++ 01_Arrays/30_Smallest_Subarray_with_sum_greater_than_a_given_value.cpp
@@ -1,4 +1,43 @@
+// easiest sliding window solution:
+int smallestSubWithSum(int arr[], int n, int x){
+    int sum = 0; int i=0; int j=0; int ans = INT_MAX; 
+    while(j < n){
+        sum += arr[j];
+        j++;
+        while(sum > x){
+            ans = min(ans, j-i);
+            sum -= arr[i];
+            i++;
+        }
+    }
+    return ans == INT_MAX ? 0 : ans;
+}
+
+
 // using sliding window technique where the window is present in between i and j
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        sort(arr, arr+n);
+        int ans = arr[n-1] - arr[0];
+
+        int large, small;
+        for(int i=1; i<n; i++){
+            if(arr[i]-k < 0){
+                continue;
+            }
+            // here we are just checking whether any other element can be larger than large
+            // or smaller than small.
+            small = min(arr[0]+k, arr[i]-k);
+            large = max(arr[n-1]-k, arr[i-1]+k);
+            
+            ans = min(ans, large - small);
+        }
+        
+        return ans;
+    }
+};
 
 
 class Solution {
