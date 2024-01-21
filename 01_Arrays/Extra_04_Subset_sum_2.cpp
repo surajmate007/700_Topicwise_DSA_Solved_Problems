160e01988191509ca4b4814691586acda15ab6da
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2024-01-21 11:49:16 +0530

    Update Extra_04_Subset_sum_2.cpp

--- 01_Arrays/Extra_04_Subset_sum_2.cpp
+++ 01_Arrays/Extra_04_Subset_sum_2.cpp
@@ -1,3 +1,34 @@
+// similar approach like combination sum 2:
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void getAns(vector<int>& nums, vector<int>& temp, int idx, int n){
+        ans.push_back(temp);
+
+        for(int i=idx; i<n; i++){
+            if(i>idx and nums[i] == nums[i-1]){
+                continue;
+            }
+            temp.push_back(nums[i]);
+            getAns(nums, temp, i+1, n);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        ans.clear();
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        getAns(nums, temp, 0, n);
+
+        return ans;
+    }
+};
+
+
+// Brute approach using set.
 class Solution {
 public:
     void function(vector<int>& nums, set<vector<int>>& st, vector<int>& ds, int n, int idx){
