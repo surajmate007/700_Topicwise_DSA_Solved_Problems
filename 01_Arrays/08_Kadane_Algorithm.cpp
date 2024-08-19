587b091dc093e0845c6ee3bba36be6b5d63b06a1
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2024-08-19 09:40:56 +0530

    Update 08_Kadane_Algorithm.cpp

--- 01_Arrays/08_Kadane_Algorithm.cpp
+++ 01_Arrays/08_Kadane_Algorithm.cpp
@@ -1,3 +1,19 @@
+// simple solution -> osum cant be negative and csum is the max value osum can reach.
+class Solution {
+public:
+    int maxSubArraySum(vector<int>& nums) {
+        int n = nums.size();
+        int csum = INT_MIN; int osum = 0;
+        for(int i=0; i<n; i++){
+            osum = max(osum, 0);
+            osum += nums[i];
+            csum = max(csum, osum);
+        }
+        return csum;
+    }
+};
+
+
 long long maxSubarraySum(int arr[], int n){
         long long csum = arr[0]; long long osum = arr[0];
         for(int i=1; i<n; i++){
