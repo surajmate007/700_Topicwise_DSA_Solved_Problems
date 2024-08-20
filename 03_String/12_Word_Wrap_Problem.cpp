4f7324e53e489c613adcfd49a363044a6bb3f745
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2024-08-20 15:47:31 +0530

    Update 12_Word_Wrap_Problem.cpp
    Revise frequently

--- 03_String/12_Word_Wrap_Problem.cpp
+++ 03_String/12_Word_Wrap_Problem.cpp
@@ -1,4 +1,42 @@
-// Using DP approach:
+// Recursion + Memoisation
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int dp[600][2010];
+    long long getAns(vector<int>& nums, int idx, int n, int width, int k){
+        if(idx >= n) return 0;
+        if(dp[idx][k] != -1) return dp[idx][k];
+        
+        long long ans1 = INT_MAX; long long ans2 = INT_MAX;
+        if(k != width) k--;
+        if(k >= nums[idx]){
+            int rem = k - nums[idx];
+            long long take = (rem * rem) + getAns(nums, idx+1, n, width, width);
+            long long notTake = getAns(nums, idx+1, n, width, rem);
+            ans1 = min(take, notTake);
+        }
+        else{
+            k++;
+            ans2 =  (k * k) + getAns(nums, idx, n, width, width);
+        }
+        
+        return dp[idx][k] = (ans1 < ans2) ? ans1 : ans2;
+    }
+    
+    int solveWordWrap(vector<int>nums, int k) { 
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        long long ans = getAns(nums, 0, n, k, k);
+        return ans;
+    }
+};
+
+
+
+// Using DP approach Top Down
 
 int solveWordWrap(vector<int>nums, int k){ 
        int n = nums.size();
