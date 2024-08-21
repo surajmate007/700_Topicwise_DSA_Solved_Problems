69932d4b9c37fa87d5d4c28f0a2f9f944ef0da6a
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2024-08-21 10:06:35 +0530

    Update 23_Search_a_Word_in_a_2D_Grid_of_characters.cpp
    easy

--- 03_String/23_Search_a_Word_in_a_2D_Grid_of_characters.cpp
+++ 03_String/23_Search_a_Word_in_a_2D_Grid_of_characters.cpp
@@ -1,6 +1,55 @@
 // This problem is similar to problem solved on leetcode but the difference is onn leetcode we can search in any direction hence the zigzag string 
 // was possible but in this problem once we finalised the direction we cant change the direction afterwards. Hence after choosing any of given 8 
 // directions then we cant change the direction and keep on searching for the next character in the same direction.
+// Here we need to return a 2d array whose indexes represents the start indexes of the word in the grid.
+
+// This solution worked:
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void getAns(vector<vector<char>>& grid, string& word, int len, int sx, int sy, int idx, int i, int j, int n, int m, int dx, int dy){
+        if(idx == len){
+            if(ans.empty()) ans.push_back({sx, sy});
+            else if(ans.back()[0] != sx or ans.back()[1] != sy) ans.push_back({sx, sy});
+            return;
+        }
+        if(i<0 or i>=n or j<0 or j>=m) return;
+        
+        if(grid[i][j] == word[idx]){
+            char ch = grid[i][j]; grid[i][j] = '0';
+            getAns(grid, word, len, sx, sy, idx+1, i+dx, j+dy, n, m, dx, dy);
+            grid[i][j] = ch;
+        }
+    }
+    
+	vector<vector<int>>searchWord(vector<vector<char>>grid, string word){
+	    int n = grid.size(); int m = grid[0].size(); int len = word.length();
+	    ans.clear();
+	    
+	    for(int i=0; i<n; i++){
+	        for(int j=0; j<m; j++){
+	            if(grid[i][j] == word[0]){
+	                getAns(grid, word, len, i, j, 0, i, j, n, m, 0, 1);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, 1, 0);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, 0, -1);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, -1, 0);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, 1, 1);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, 1, -1);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, -1, 1);
+    	            getAns(grid, word, len, i, j, 0, i, j, n, m, -1, -1);
+	            }
+	        }
+	    }
+	    
+	    return ans;
+	}
+};
+
+
+
 
 // solution giving proper results for every testcase but gfg saying it wrong.
 
